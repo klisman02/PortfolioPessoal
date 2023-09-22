15671d935585b928ceb1a3a33d691e7e985bbfd5
Klisman Gomes <76626836+klisman02@users.noreply.github.com>
2023-09-22 16:41:18 -0300

    Add files via upload

---
+++ Portifólio Pessoal/js/script.js
@@ -0,0 +1,7 @@
+// TYPING ANIMATION
+var typed = new Typed(".typing", {
+    strings:["","Web Designer", "Desenvolvedor Front-End", "Desenvolvedor Web"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
